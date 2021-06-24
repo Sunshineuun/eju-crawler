1c9b4f414d0dcad74b1f9a574500dc6c10b3d795
Sunshineuun <qiushengming@aliyun.com>
2021-06-24 20:18:32 +0800

    符号常量类

---
+++ src/main/java/com/qiusm/eju/crawler/constant/SymbolicConstant.java
@@ -0,0 +1,21 @@
+package com.qiusm.eju.crawler.constant;
+
+/**
+ * @author qiushengming
+ */
+public class SymbolicConstant {
+    /**
+     * 逗号【,】
+     */
+    public static final String COMMA = ",";
+
+    /**
+     * 分号【;】
+     */
+    public static final String SEMICOLON = ";";
+
+    /**
+     * 【\|】
+     */
+    public static final String PIPE = "\\|";
+}
