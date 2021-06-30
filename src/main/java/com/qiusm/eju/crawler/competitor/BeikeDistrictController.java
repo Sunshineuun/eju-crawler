d57a0a95bf31bd1c5dbe5dcca422f312659d802d
Sunshineuun <qiushengming@aliyun.com>
2021-06-30 14:11:58 +0800

    bk 围栏数据

--- src/main/java/com/qiusm/eju/crawler/competitor/BeikeDistrictController.java
+++ src/main/java/com/qiusm/eju/crawler/competitor/BeikeDistrictController.java
@@ -1,6 +1,7 @@
 package com.qiusm.eju.crawler.competitor;
 
 import com.qiusm.eju.crawler.competitor.beike.BeikeDistrictService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +12,7 @@ import javax.annotation.Resource;
 /**
  * @author qiushengming
  */
+@Slf4j
 @RestController
 @RequestMapping("/bk")
 public class BeikeDistrictController {
@@ -21,6 +23,7 @@ public class BeikeDistrictController {
     @GetMapping("/city/{cityName}")
     public void city(@PathVariable String cityName) {
         districtService.city(cityName);
+        log.info("{}-区域商圈围栏数据处理完毕！", cityName);
     }
 
     @GetMapping("/testCity/{cityCode}")
