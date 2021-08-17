22ecd23e8ec4cfaa069a8377536b0a3c2bc4f957
qiushengming <qiushengming@aliyun.com>
2021-08-17 18:26:05 +0800

    增加代理配置入口

--- src/main/java/com/qiusm/eju/crawler/service/bk/BeikeBaseService.java
+++ src/main/java/com/qiusm/eju/crawler/service/bk/BeikeBaseService.java
@@ -1,13 +1,15 @@
 package com.qiusm.eju.crawler.service.bk;
 
+import com.qiusm.eju.crawler.service.base.IProxyUrlService;
 import com.qiusm.eju.crawler.utils.http.OkHttpUtils;
+import org.springframework.beans.factory.InitializingBean;
 
+import javax.annotation.Resource;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import static com.qiusm.eju.crawler.constant.CharacterSet.GBK;
-import static com.qiusm.eju.crawler.constant.EjuConstant.PROXY_URL0;
 import static com.qiusm.eju.crawler.constant.SymbolicConstant.COMMA;
 
 /**
@@ -15,13 +17,26 @@ import static com.qiusm.eju.crawler.constant.SymbolicConstant.COMMA;
  *
  * @author qiushengming
  */
-public abstract class BeikeBaseService {
+public abstract class BeikeBaseService implements InitializingBean {
+
+    @Resource
+    private IProxyUrlService proxyUrlService;
     protected static final String BEIKE_FILE_ROOT = "source/beike/";
 
-    protected OkHttpUtils httpUtils = OkHttpUtils.Builder().proxyUrl(PROXY_URL0).connectTimeout(60000).readTimeout(60000).charset(GBK).builderHttp();
+    protected OkHttpUtils httpUtils;
     protected final List<String> ERROR_MSG = new ArrayList<>();
 
     {
         ERROR_MSG.addAll(Arrays.asList("ejuResponseCode=500,ResponseCode=,ResponseError=,枌~怣秙".split(COMMA)));
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        httpUtils = OkHttpUtils.Builder()
+                .proxyUrl(proxyUrlService.getHttpBaseProxyUrl())
+                .connectTimeout(60000)
+                .readTimeout(60000)
+                .charset(GBK)
+                .builderHttp();
     }
 }
