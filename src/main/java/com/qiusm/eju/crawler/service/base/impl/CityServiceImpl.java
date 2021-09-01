c10919f1328b58dbc4585dc89e6106497df37de6
qiushengming <qiushengming@aliyun.com>
2021-09-01 09:25:08 +0800

    增加bk city code 的查询方法

--- src/main/java/com/qiusm/eju/crawler/service/base/impl/CityServiceImpl.java
+++ src/main/java/com/qiusm/eju/crawler/service/base/impl/CityServiceImpl.java
@@ -1,5 +1,6 @@
 package com.qiusm.eju.crawler.service.base.impl;
 
+import com.baomidou.mybatisplus.mapper.EntityWrapper;
 import com.baomidou.mybatisplus.service.impl.ServiceImpl;
 import com.qiusm.eju.crawler.entity.base.City;
 import com.qiusm.eju.crawler.mapper.base.CityMapper;
@@ -10,4 +11,11 @@ import org.springframework.stereotype.Service;
 public class CityServiceImpl
         extends ServiceImpl<CityMapper, City>
         implements ICityService {
+
+    @Override
+    public City selectByBkCityCode(String cityCode) {
+        EntityWrapper<City> wrapper = new EntityWrapper<>();
+        wrapper.eq("bk_code", cityCode);
+        return this.selectOne(wrapper);
+    }
 }
