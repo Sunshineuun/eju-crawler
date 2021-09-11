94d12239e8c1b78f2678e2781f68b645a61eba3b
qiushengming <qiushengming@aliyun.com>
2021-09-11 22:01:58 +0800

    代码优化

--- src/main/java/com/qiusm/eju/crawler/controller/base/CrawlerTaskSchedulingBaseController.java
+++ src/main/java/com/qiusm/eju/crawler/controller/base/CrawlerTaskSchedulingBaseController.java
@@ -1,41 +1,8 @@
 package com.qiusm.eju.crawler.controller.base;
 
-import com.qiusm.eju.crawler.entity.task.CrawlerTaskInstance;
-import com.qiusm.eju.crawler.service.task.ICrawlerTaskInstanceService;
-import com.qiusm.eju.crawler.service.task.ICrawlerTaskService;
-import com.qiusm.eju.crawler.utils.ThreadUtils;
-
-import javax.annotation.Resource;
-
 /**
  * @author qiushengming
  */
 public abstract class CrawlerTaskSchedulingBaseController {
 
-    @Resource
-    protected ICrawlerTaskInstanceService crawlerTaskInstanceService;
-
-    /**
-     * 任务调度
-     */
-    public void scheduling() {
-        Thread taskSchedulingThread = new Thread(() -> {
-            while (true) {
-                CrawlerTaskInstance var = crawlerTaskInstanceService.getInstanceByHandlerType(this.getClass().getSimpleName());
-                if (var != null) {
-                    start(var);
-                }
-                ThreadUtils.sleep(60);
-            }
-        });
-        taskSchedulingThread.setName(this.getClass().getSimpleName() + "task-scheduling");
-        taskSchedulingThread.start();
-    }
-
-    /**
-     * 子类启动的方法
-     *
-     * @param crawlerTaskInstance 任务实例
-     */
-    public abstract void start(CrawlerTaskInstance crawlerTaskInstance);
 }
