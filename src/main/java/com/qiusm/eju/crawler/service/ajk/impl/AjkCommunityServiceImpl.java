4a12a896f455ab266f9a67c09e02a256bed5c866
qiushengming <qiushengming@aliyun.com>
2021-09-18 20:06:37 +0800

    增加ajk小区详情页抓取

--- src/main/java/com/qiusm/eju/crawler/service/ajk/impl/AjkCommunityServiceImpl.java
+++ src/main/java/com/qiusm/eju/crawler/service/ajk/impl/AjkCommunityServiceImpl.java
@@ -5,6 +5,7 @@ import com.alibaba.fastjson.JSONObject;
 import com.qiusm.eju.crawler.dto.RequestDto;
 import com.qiusm.eju.crawler.dto.ResponseDto;
 import com.qiusm.eju.crawler.entity.base.Community;
+import com.qiusm.eju.crawler.parser.competitor.anjuke.app.community.CommunityDetailHandler;
 import com.qiusm.eju.crawler.parser.competitor.anjuke.app.community.CommunityListHandler;
 import com.qiusm.eju.crawler.parser.competitor.anjuke.app.community.CommunityPageListHandler;
 import com.qiusm.eju.crawler.service.ajk.IAjkCommunityService;
@@ -31,6 +32,8 @@ public class AjkCommunityServiceImpl
     private CommunityListHandler communityListHandler;
     @Resource
     private ICommunityService communityService;
+    @Resource
+    private CommunityDetailHandler communityDetailHandler;
 
     @Override
     public JSONArray communityList(JSONObject area) {
@@ -53,13 +56,18 @@ public class AjkCommunityServiceImpl
         return result;
     }
 
+    @Override
+    public void communityDetail(Community community) {
+        communityDetailHandler(community);
+    }
+
     /**
      * 获取当前区域下的翻页列表
      *
      * @param area 区域信息
      * @return 翻页列表
      */
-    JSONArray pageListHandler(JSONObject area) {
+    private JSONArray pageListHandler(JSONObject area) {
         Map<String, String> params = new HashMap<>(8);
         params.put(CITY_ID, area.getString(CITY_ID));
         params.put(AREA_ID, area.getString(AREA_ID));
@@ -80,7 +88,7 @@ public class AjkCommunityServiceImpl
      * @param page 当前页面的请求参数
      * @return 小区列表
      */
-    JSONArray pageHandler(JSONObject page) {
+    private JSONArray pageHandler(JSONObject page) {
         Map<String, String> params = new HashMap<>(8);
         params.put(CITY_ID, page.getString(CITY_ID));
         params.put(AREA_ID, page.getString(AREA_ID));
@@ -97,6 +105,29 @@ public class AjkCommunityServiceImpl
         ResponseDto responseDto = communityListHandler.execute(requestDto);
 
         return responseDto.getResult().getJSONArray("list");
+    }
+
+    private void communityDetailHandler(Community community) {
+        Map<String, String> params = new HashMap<>(8);
+        params.put("comm_id", community.getCommunityId());
+        params.put("city_id", community.getCityId());
+
+        RequestDto requestDto = RequestDto.builder()
+                .requestParam(params)
+                .build();
+        ResponseDto responseDto = communityDetailHandler.execute(requestDto);
+        JSONObject json = responseDto.getResult();
+        if (json != null) {
+            community.setPropertyCompany(json.getString("property_company"));
+            community.setPropertyExpenses(json.getString("property_money"));
+            community.setVolumeRate(json.getString("plot_ratio"));
+            community.setGreeningRate(json.getString("landscaping_ratio"));
+            community.setParkSpace(json.getString("parking"));
+            community.setDevEnt(json.getString("developer"));
+            community.setBuildYear(json.getString("completion_time"));
+            community.setTotalArea(json.getString("total_area"));
+            community.setTotalHouse(json.getString("total_house_hold_num"));
+        }
     }
 
     private void toCommunityDb(JSONArray communityList) {
