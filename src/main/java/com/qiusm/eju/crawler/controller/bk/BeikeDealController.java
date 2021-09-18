792f3c0a0348d41a6a0ab5d9aa4de4f5a4cba990
qiushengming <qiushengming@aliyun.com>
2021-09-18 17:19:27 +0800

    代码优化

--- src/main/java/com/qiusm/eju/crawler/controller/bk/BeikeDealController.java
+++ src/main/java/com/qiusm/eju/crawler/controller/bk/BeikeDealController.java
@@ -1,6 +1,6 @@
 package com.qiusm.eju.crawler.controller.bk;
 
-import com.qiusm.eju.crawler.entity.task.CrawlerTaskInstance;
+import com.qiusm.eju.crawler.parser.competitor.beike.pc.BkDealHandler;
 import com.qiusm.eju.crawler.service.bk.IBkDealTaskService;
 import io.swagger.annotations.Api;
 import lombok.extern.slf4j.Slf4j;
@@ -23,7 +23,8 @@ public class BeikeDealController extends BeiKeBaseController {
     @Resource
     private IBkDealTaskService bkDealTaskService;
 
-    private CrawlerTaskInstance nowTask;
+    @Resource
+    private BkDealHandler handler;
 
     /**
      * 开始执行任务，任务配置在表中
@@ -39,5 +40,16 @@ public class BeikeDealController extends BeiKeBaseController {
     @GetMapping("/start/toDb")
     public void startToDb() {
         bkDealTaskService.toDb();
+    }
+
+    /**
+     * @param city 城市简拼，参考贝壳
+     */
+    @GetMapping("/start/pc/task")
+    public void startPcTask(String city) {
+        for (String c : city.split(",")) {
+            log.info("开始执行：{}", c);
+            handler.handler(c);
+        }
     }
 }
